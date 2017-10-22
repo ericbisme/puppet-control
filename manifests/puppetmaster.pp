09a52d18070452380fb97b67c98bb398377a9c8f
Eric Bolinger <ericb@ericbisme.net>
2017-10-22 14:38:13 -0600

    updates for r10k webhook

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -13,3 +13,18 @@ class { 'r10k':
 class { 'r10k::mcollective':
   ensure => false,
 }
+
+# Instead of running via mco, run r10k directly
+class {'r10k::webhook::config':
+  use_mcollective => false,
+}
+
+# The hook needs to run as root when not running using mcollective
+# It will issue r10k deploy environment <branch_from_gitlab_payload> -p
+# When git pushes happen.
+class {'r10k::webhook':
+  use_mcollective => false,
+  user            => 'root',
+  group           => '0',
+  require         => Class['r10k::webhook::config'],
+}
