df57b988cd4442dcc45f0584605a4ef2df75c758
Eric Bolinger <ericb@ericbisme.net>
2017-10-22 14:11:04 -0600

    ordering in puppetmaster manifest

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,10 +1,10 @@
 class { 'r10k':
-  sources => {
+  provider   => 'puppet_gem',
+  configfile => '/etc/puppetlabs/r10k/r10k.yaml',
+  sources   => {
     'ericbisme'  => {
       'remote'     => 'https://github.com/ericbisme/puppet-control.git',
       'basedir'    => "/etc/puppetlabs/code/environments",
-      'provider'   => 'puppet_gem',
-      'configfile' => '/etc/puppetlabs/r10k/r10k.yaml',
       'prefix'     => false,
     },
   },
