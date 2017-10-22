bea6a85d44bdbbef97fec4ae5cfbe13133c3ad49
Eric Bolinger <ericb@ericbisme.net>
2017-10-22 14:27:46 -0600

    test webhook

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -2,10 +2,14 @@ class { 'r10k':
   provider   => 'puppet_gem',
   configfile => '/etc/puppetlabs/r10k/r10k.yaml',
   sources   => {
-    'ericbisme'  => {
+    'puppet'  => {
       'remote'     => 'https://github.com/ericbisme/puppet-control.git',
       'basedir'    => "/etc/puppetlabs/code/environments",
       'prefix'     => false,
     },
   },
+}
+
+class { 'r10k::mcollective':
+  ensure => false,
 }
