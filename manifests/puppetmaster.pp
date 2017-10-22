7af1ea9734914edd53fea07a471b634694311da7
Eric Bolinger <ericb@ericbisme.net>
2017-10-22 17:06:33 -0600

    remove comments

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -14,16 +14,12 @@ class { 'r10k::mcollective':
   ensure => false,
 }
 
-# Instead of running via mco, run r10k directly
 class {'r10k::webhook::config':
   use_mcollective => false,
   public_key_path  => '/etc/mcollective/server_public.pem',  # Mandatory for FOSS
   private_key_path => '/etc/mcollective/server_private.pem', # Mandatory for FOSS
 }
 
-# The hook needs to run as root when not running using mcollective
-# It will issue r10k deploy environment <branch_from_gitlab_payload> -p
-# When git pushes happen.
 class {'r10k::webhook':
   use_mcollective => false,
   user            => 'root',
